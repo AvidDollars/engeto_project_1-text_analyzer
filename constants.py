2c08c712c08d9d113f01ca3c1e23a800be4c5b1e
AvidDollars <dima.kol@seznam.cz>
2024-06-02 23:55:22 +0200

    add file with constants

---
+++ constants.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+PROJECTS_ROOT = Path(__file__).parent
+DATA_DIR = PROJECTS_ROOT.joinpath("data")
+AUTH_USERS_JSON = DATA_DIR.joinpath("users.json").resolve()
+TEXT_1 = DATA_DIR.joinpath("text_1.txt").resolve()
+TEXT_2 = DATA_DIR.joinpath("text_2.txt").resolve()
+TEXT_3 = DATA_DIR.joinpath("text_3.txt").resolve()
+HR_LINE = 40 * "-"
